03166dcc49f58ac947bc8dbbf0007bf925282fc2
Akash Kulkarni <kulkarniakash32@gmail.com>
2024-06-25 15:16:56 +0530

    display flex to button

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <div className="container w-4/5 h-4/6 rounded-xl mx-auto my-5 pt-7 bg-violet-400 min-h-[80vh]">
+      <div className="container w-screen md:w-4/5 h-4/6 rounded-xl mx-auto my-5 pt-7 bg-violet-400 min-h-[80vh]">
         <div className="logo text-center font-bold text-white text-2xl font-serif ">
           <h1>Add a Todo</h1>
           <input type="text" value={Todo} className='text-black max-w-[45vw]' onChange={handleInput} onFocus={handlFocus} onBlur={handleBlur} /> <button className='bg-green-500 rounded-lg p-1 text-xl' onClick={handleAdd} >Add</button>
@@ -79,14 +79,14 @@ function App() {
           <div className="todos mt-5">
             {todos.length === 0 && <div className='mx-auto text-xl font-semibold text-center'> No Todos to display!</div>}
           {todos.map(items=>{
-            return (showFinished || !items.isCompleted) && ((<div className="todo flex justify-around gap-6 my-2 font-medium" key={items.id}>
-              <div className='flex  flex-start gap-2 min-w-[36%]'>
+            return (showFinished || !items.isCompleted) && ((<div className="todo flex md:justify-around gap-6 my-2 font-medium justify-between px-4" key={items.id}>
+              <div className='flex  flex-start gap-2 min-w-[36%] max-w-[50%]'>
                 <input type="checkbox" name={items.id} id="" checked={items.isCompleted} onChange={handleCheckbox} />
               <div className={items.isCompleted? "text line-through":"text"}>{items.Todo}</div>
               </div>
-              <div>
-              <button className='bg-green-500 rounded-md px-1 ' onClick={handleEdit} name={items.id}><FaRegEdit style={{pointerEvents:'none'}} className='w-6 h-6' /></button>
-              <button className='bg-green-500 rounded-md px-1 mx-2'onClick={handleRemove} name={items.id}><MdDelete style={{pointerEvents:'none'}} className='w-6 h-6'/> </button>
+              <div className='flex'>
+              <button className='bg-green-500 rounded-md px-1 max-h-6' onClick={handleEdit} name={items.id}><FaRegEdit style={{pointerEvents:'none'}} className='w-6 h-6' /></button>
+              <button className='bg-green-500 rounded-md px-1 mx-2 max-h-6'onClick={handleRemove} name={items.id}><MdDelete style={{pointerEvents:'none'}} className='w-6 h-6'/> </button>
               </div>
             </div>))
           })}
